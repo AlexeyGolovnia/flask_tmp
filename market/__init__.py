fd2e7b1451ac150aead852c3d594a5b658885b99
Alexey Golovnia <fumeron@mail.ru>
2022-12-05 22:12:57 +0300

    221205_3

--- market/__init__.py
+++ market/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 # from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@host.docker.internal:5432/market'
@@ -9,5 +10,6 @@ app.config['SECRET_KEY'] = '933a8266efc1bb729e490bdb'
 db = SQLAlchemy(app)
 
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from market import routes
